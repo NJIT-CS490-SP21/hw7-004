e68c34f4070483f566a95287d8453d8652df69c2
EC2 Default User <ec2-user@ip-172-31-48-203.ec2.internal>
2021-02-09 15:21:54 +0000

    Added list to app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-
-@app.route("/")
+global student_lst
+student_lst = []
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    global student_lst
+    if request.method == 'POST' and "student_btn" in request.form:
+        student = request.form['student']
+        student_lst.append(student)
+    return render_template("index.html", student_lst=student_lst)
 
 app.run(
     port=int(os.getenv("PORT", "8080")),
