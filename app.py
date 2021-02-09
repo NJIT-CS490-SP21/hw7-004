818c70a90be7acf2d0d78f6f5d0a5a7fc2fd4e9f
michaelcbarilla <michaelcbarilla@gmail.com>
2021-02-09 15:18:27 +0000

    Added random number in index.html/ added random number data in app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        random_number = random.randint(0,1000)
+    )
 
 app.run(
     port=int(os.getenv("PORT", "8080")),
